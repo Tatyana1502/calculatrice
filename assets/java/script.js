3ee7abf2f6d3a2bee386104cca1add6448726810
13010-34-03 <13010-34-03@exchange.ad.afpanet>
2023-10-12 16:55:07 +0200

    first commit

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,32 +1,71 @@
-let maTable = document.createElement("table");
-document.body.appendChild(maTable);
-maTable.appendChild(Ecran);
-maTable.appendChild(Zero);
-maTable.appendChild(Numero);
-maTable.appendChild(Signe);
-Memory
-Rezultat
-const letters = ["A", "B", "C", "D"];
-for (let nbRows = 0; nbRows <= 3; nbRows++) {
-    let uneRow = document.createElement("tr");
-    maTable.appendChild(uneRow);
-    for (let nbColumn = 0; nbColumn <= 4; nbColumn++) {
-        let uneColumn = document.createElement("td");
-        // uneColumn.setAttribute("id", letters[nbColumn] + parseInt(nbRows))
-        if (nbColumn = 1 && nbRows = 1) {
-              uneColumn.classList.add("Numero");
-        // uneColumn.setAttribute("id", letters[nbColumn] + parseInt(nbRows))
-            if (nbColumn = 0 && nbRows < 3) {
-                uneColumn.classList.add("Zero");
-            }
-            if (nbColumn > 0 && nbRows = 3) {
-                uneColumn.classList.add("Signe");
-            }
-        }
-
-        uneRow.appendChild(uneColumn);
+// var button = document.querySelector("button");
+// button.addEventListener("click", function() {console.log("oui")});
+// if (Numero >= 0) {
+// oninput.Number = ecran;
+// }
+var op;
+function func() {
+    var Rezultat;
+    var num1 = Number(document.getElementById(num1).value);
+    var num2 = Number(document.getElementById(num1).value);
+    // var Rezultat = Number(document.getElementById(num1).value);
+    switch (op) {
+        case '+':
+            Rezultat = num1 + num2;
+            break;
+        case '-':
+            Rezultat = num1 - num2;
+            break;
+        case '*':
+            Rezultat = num1 * num2;
+            break; 
+        case '/':
+            Rezultat = num1 / num2;
+            break;  
+       
+        // document.getElementById("Rez").innerHTML = ecran;          
     }
+        //  if (Rezultat <=0) {
+        //     //  oninput id="rez.value";
+        //  }
+    document.getElementById("Rezultat").innerHTML = Rezultat;
 }
+
+
+// let maTable = document.createElement("table");
+// document.body.appendChild(maTable);
+// // maTable.appendChild(Ecran);
+// // maTable.appendChild(Zero);
+// // maTable.appendChild(Numero);
+// // maTable.appendChild(Signe);
+// // Memory
+// // Rezultat
+// const letters = ["A", "B", "C", "D"];
+// for (let nbRows = 0; nbRows <= 3; nbRows++) {
+//     let uneRow = document.createElement("tr");
+//     maTable.appendChild(uneRow);
+//     for (let nbColumn = 0; nbColumn <= 4; nbColumn++) {
+//         let uneColumn = document.createElement("td");
+        
+
+//         // uneColumn.setAttribute("id", letters[nbColumn] + parseInt(nbRows))
+//         if (nbColumn = 0 && nbRows >= 2) {
+//               uneColumn.classList.add("numero");
+//         }
+//         // uneColumn.setAttribute("id", letters[nbColumn] + parseInt(nbRows))
+//             if (nbColumn = 0 && nbRows >= 4) {
+//                 uneColumn.classList.add("zero");
+//             }
+//             if (nbColumn = 3 && nbRows >= 3) {
+//                 uneColumn.classList.add("signe");
+//             }
+//         }
+     
+        
+    
+//     uneRow.appendChild(uneColumn);
+// }
+// }
 // let maTable = document.createElement("table");
 // document.body.appendChild(maTable);
 // const letters = ["A", "B", "C", "D", "E","F", "G", "H"];
